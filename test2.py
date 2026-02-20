8eedefb271bce29a7a147d575046cd9c45723f0a
TRUSHNA-tech <125817679+TRUSHNA-tech@users.noreply.github.com>
2026-02-20 14:04:07 +0530

    test

--- test2.py
+++ test2.py
@@ -1,1 +1,16 @@
-print("hello world")
+import matplotlib.pyplot as plt
+
+# Sample data
+x = [1, 2, 3, 4, 5]
+y = [10, 20, 25, 30, 40]
+
+# Create line plot
+plt.plot(x, y)
+
+# Adding labels and title
+plt.xlabel("X Axis")
+plt.ylabel("Y Axis")
+plt.title("Simple Line Graph")
+
+# Show graph
+plt.show()
